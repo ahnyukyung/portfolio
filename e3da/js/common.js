56f91ed674dc2d97e64c924e24fda93ae1333a5a
ahnyukyung <dksdbrud123@gmail.com>
2022-01-27 21:29:18 +0900

    modify e3da

--- e3da/js/common.js
+++ e3da/js/common.js
@@ -1,10 +1,34 @@
-let gnbTit = document.querySelectorAll(".gnb_tit");
-
-function gnbOpen() {
-  const gnbBg = document.querySelector(".gnb_bg");
-
-  gnbBg.classList.add("open");
-}
-[].forEach.call(gnbTit, function (gnbTit) {
-  gnbTit.addEventListener("mouseenter", gnbOpen);
+$(".gnb >li > a")
+  .on("mouseenter", function () {
+    $(".header_inner").addClass("open");
+    $(".header_inner h1 img").attr("src", "images/logo_02.svg");
+  })
+  .on("mouseleave", function () {
+    $(".header_inner").removeClass("open");
+    $(".header_inner h1 img").attr("src", "images/logo_01.svg");
+  });
+$(".depth02 a")
+  .on("mouseenter", function () {
+    $(this).parents("li").addClass("active");
+    $(this).parents(".depth02").addClass("active");
+  })
+  .on("mouseleave", function () {
+    $(this).parents("li").removeClass("active");
+    $(this).parents(".depth02").removeClass("active");
+  });
+$(".depth02").on("mouseover", function () {
+  $(".gnb >li > a").trigger("mouseenter");
+});
+$(".depth02").on("mouseleave", function () {
+  $(".gnb >li > a").trigger("mouseleave");
+});
+$(".m_gnb_tit").on("click", function (event) {
+  event.preventDefault();
+  $(this).toggleClass("active").siblings("ul").slideToggle("fast");
+});
+$(".gnb_open").on("click", function () {
+  $("body").css("overflow", "hidden");
+});
+$(".gnb_close").on("click", function () {
+  $("body").css("overflow", "auto");
 });
