5f075a15764caaac18de6acb2a89f0745a393163
ahnyukyung <dksdbrud123@gmail.com>
2022-03-14 10:09:59 +0900

    modify e3da

--- e3da/js/front_36.js
+++ e3da/js/front_36.js
@@ -2,5 +2,7 @@ $(".answer ").hide();
 $(".question").on("click", function (e) {
   e.preventDefault();
   $(this).siblings(".answer").slideToggle();
+  $(this).parent().siblings().children(".answer").slideUp();
+  $(this).parent().siblings().children(".question").removeClass("active");
   $(this).toggleClass("active");
 });
