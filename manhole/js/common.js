3f747a8d461bce15c18d93386f6acba816394648
ahnyukyung <dksdbrud123@gmail.com>
2022-06-28 21:11:26 +0900

    modify manhole

---
+++ manhole/js/common.js
@@ -0,0 +1,7 @@
+$(function () {
+  // sidebar 클릭 시 하위메뉴 슬라이드
+  // $(".panel_box").not(".panel_box:nth-child(1)").hide();
+  // $(".panel_box .sub_panel").hide();
+
+  $("#sidebar .navibar_list li").click(function () {});
+});
